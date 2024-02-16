0d869ed81c5ba9433a25346b8d2d965866d82101
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2024-02-16 11:50:23 +0100

    multi-dimensional array

--- multi_dimensional_array.php
+++ multi_dimensional_array.php
@@ -10,11 +10,26 @@ echo "<br>";
 // creating a 2 dimension array
 $multiDim = array(array(1,2,3,4),
                  array(5,6,7,8),
-                 array(9,10,11,12));
+                 array(9,0,1,2));
                  
 //echo var_dump($multiDim);
 
-echo $multiDim[0][2];
+//echo $multiDim[0][2];
+ 
+// PRINTING THE CONTENTS OF A 2 DIMENSIONAL ARRAY
+for ($i=0; $i < count($multiDim) ; $i++) { 
+    echo var_dump($multiDim[$i]);
+    echo "<br>";
+}
+
+for ($i=0; $i < count($multiDim) ; $i++) { 
+    for ($j=0; $j < count($multiDim[$i]); $j++) { 
+
+        echo $multiDim[$i][$j];
+        echo " ";
+    }
+    echo "<br>";
+}
 
 
 
