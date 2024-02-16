b2d4eb3663eba16fd360914c9618745146bb2b98
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2024-02-16 11:32:51 +0100

    1 dimension array

---
+++ multi_dimensional_array.php
@@ -0,0 +1,13 @@
+<?php
+echo "multi-dim";
+
+echo "<br>";
+
+$favCol = array('juha' => 'red','archa' => 'blue','maruf' => 'black');
+foreach ($favCol as $key => $value) {
+    echo "<br> Fav color of $key is $value";
+    
+}
+
+
+?>
