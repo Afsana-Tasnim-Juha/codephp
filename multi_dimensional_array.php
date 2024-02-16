e5eef4275cc9f90a69090951c45b963f399939cb
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2024-02-16 11:40:47 +0100

    2 dimension array

--- multi_dimensional_array.php
+++ multi_dimensional_array.php
@@ -2,12 +2,24 @@
 echo "multi-dim";
 
 echo "<br>";
+ // one dimension array
+//$favCol = array('juha' => 'red','archa' => 'blue','maruf' => 'black');
+//foreach ($favCol as $key => $value) {
+    //echo "<br> Fav color of $key is $value";
 
-$favCol = array('juha' => 'red','archa' => 'blue','maruf' => 'black');
-foreach ($favCol as $key => $value) {
-    echo "<br> Fav color of $key is $value";
+// creating a 2 dimension array
+$multiDim = array(array(1,2,3,4),
+                 array(5,6,7,8),
+                 array(9,10,11,12));
+                 
+//echo var_dump($multiDim);
+
+echo $multiDim[0][2];
+
+
+
     
-}
+
 
 
 ?>
